2a5673b1edd0b3e168c8fd6c91f48791e2002f7c
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2023-02-18 08:34:51 +0530

    requirements.txt

--- deploy.py
+++ deploy.py
@@ -4,5 +4,5 @@ import shubhlipi as sh
 
 for x in sh.argv:
     if x == "push":
-        sh.cmd("cd py && python3 karah.py deploy", direct=False)
+        sh.cmd("cd py && python3 karah.py requirements.txt", direct=False)
         sh.cmd("space push", direct=False)
