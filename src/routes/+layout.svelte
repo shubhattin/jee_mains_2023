a5fb2fafacf68e98cd10ee590d1bb7dd58c5f0a4
Shubham Anand Gupta <shubhattin@users.noreply.github.com>
2023-04-18 03:39:09 -0700

    added font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import '../app.postcss';
+  import '@fontsource/roboto/latin.css';
   import { onMount } from 'svelte';
   import PartyTown from '@components/tags/PartyTown.svelte';
   import { change_html_lang_tag } from '@tools/i18n';
