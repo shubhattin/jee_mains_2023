2ad486dac78eccd2712000f1db73c67c810c9afc
Shubham Anand Gupta <shubhattin@users.noreply.github.com>
2023-04-19 10:22:37 -0700

    h

--- api/jee/process_data/get_data.go
+++ api/jee/process_data/get_data.go
@@ -170,6 +170,15 @@ func GetResult(data *MainDataType) ResultDataType {
 	}
 	for i := 0; i < 3; i++ {
 		dt.Subjects[i].Score = int16(len(dt.Subjects[i].Correct)*4 - len(dt.Subjects[i].Incorrect))
+		if len(dt.Subjects[i].Incorrect) == 0 {
+			dt.Subjects[i].Incorrect = make([]string, 0)
+		}
+		if len(dt.Subjects[i].Correct) == 0 {
+			dt.Subjects[i].Correct = make([]string, 0)
+		}
+		if len(dt.Subjects[i].Unattempted) == 0 {
+			dt.Subjects[i].Unattempted = make([]string, 0)
+		}
 	}
 	dt.Score = int16(4*dt.CorrectCount - 1*dt.IncorrectCount)
 	if TEST_MODE {
